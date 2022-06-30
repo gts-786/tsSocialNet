d890454d914918e510e13b7d0d069365dda5dc06
Roman Markovich <gts-786@mail.ru>
2022-06-30 12:39:00 +0300

    Add opening post by click

--- src/redux/ActionCreator.ts
+++ src/redux/ActionCreator.ts
@@ -3,18 +3,17 @@ import { IPost } from './../tools/models/IPost';
 import axios from "axios";
 import { postSlice } from "./PostSlice";
 import { AppDispatch } from "./store";
-import { useParams } from 'react-router-dom';
 
 export const fetchPosts = (limit: number, page: number, setTotalPages: any) => async (dispatch: AppDispatch) => {
     try {
         dispatch(postSlice.actions.postsFetching());
         const response = await axios.get<IPost[]>('https://jsonplaceholder.typicode.com/posts',
-        {
-            params: {
-                _limit: limit,
-                _page: page
-            }
-        })
+            {
+                params: {
+                    _limit: limit,
+                    _page: page
+                }
+            })
         dispatch(postSlice.actions.postsFetchingSuccess(response.data));
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit))
@@ -24,21 +23,9 @@ export const fetchPosts = (limit: number, page: number, setTotalPages: any) => a
     }
 }
 
-export const fetchPostsById = (id: number) => async (dispatch: AppDispatch) => {
-    try {
-        dispatch(postSlice.actions.postsFetching());
-        const response = await axios.get<IPost>(`https://jsonplaceholder.typicode.com/posts/${id}`,
-        {
-            params: {
-                _id: id
-            }
-        })
-        dispatch(postSlice.actions.postsFetchingByIdSuccess(response.data));
-        console.log(response.data);
-    }
-    catch (error) {
-        dispatch(postSlice.actions.postsFetchingError((error as Error).message));
-    }
+export const fetchPostsById = (id: any) => async () => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    return (response.data);
 }
 
 /* export const addPost = (dispatch: AppDispatch, title: string, body: string) => {
