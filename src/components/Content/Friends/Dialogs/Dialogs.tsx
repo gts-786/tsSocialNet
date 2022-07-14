26005b6909efac1ddc83a284dbea6420f6e5f54c
Roman Markovich <gts-786@mail.ru>
2022-07-14 13:57:00 +0300

    Add Dialogs to Friends

--- src/components/Content/Friends/Dialogs/Dialogs.tsx
+++ src/components/Content/Friends/Dialogs/Dialogs.tsx
@@ -1,14 +1,19 @@
 import { useAppSelector } from "../../../../tools/hooks/redux";
 import FormMessage from "./FormMessage";
-import ava from "./../../../../tools/images/ava.png";
 import classes from "./Dialogs.module.css";
+import { FC } from "react";
 
-const Dialogs = () => {
+interface DialogsProps {
+    dialogUser: any;
+}
+
+const Dialogs: FC<DialogsProps> = ({dialogUser}) => {
     const { messages } = useAppSelector(state => state.messageReducer)
     return (
         <div className={classes.dialogs}>
             <div>
-                <img src={ava} alt="Picture" className={classes.img} />
+                <img src={dialogUser.image} alt="Picture" className={classes.img} />
+                <div>{dialogUser.name}</div>
             </div>
             <div>
                 {messages.map(message => <div key={message.id}>{message.message}</div>)}
--- src/components/Content/Messages/Addressee/UsersMessages.tsx
+++
@@ -1,8 +0,0 @@
-const UsersMessages = () => {
-    return (
-        <div>
-            Здесь будет реализована логика открытия полльзователя и диалога с ним
-        </div>
-    );
-};
-export default UsersMessages;
