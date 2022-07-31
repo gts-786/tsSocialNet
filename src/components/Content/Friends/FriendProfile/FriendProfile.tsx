f1b76480c39521e3c39a2e4b5d9e32443968db9a
Roman Markovich <gts-786@mail.ru>
2022-07-31 15:48:35 +0300

    FriendProfile is ready

--- src/components/Content/Friends/FriendProfile/FriendProfile.tsx
+++ src/components/Content/Friends/FriendProfile/FriendProfile.tsx
@@ -1,20 +1,65 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import QueryPosts from "../../../../server/QueryPosts";
+import { useFetching } from "../../../../tools/hooks/useFetching";
+import { IPost } from "../../../../tools/models/IPost";
 import classes from './FriendProfile.module.css';
 
 interface FriendProfileProps {
     profileUser: any
 }
 
-const FriendProfile: FC<FriendProfileProps> = ({profileUser}) => {
+const FriendProfile: FC<FriendProfileProps> = ({ profileUser }) => {
+    //Код для запроса постов для создания красоты в приложении 
+    const [posts, setPosts] = useState<IPost[]>([])
+    const limit = profileUser.id * 3 - 1
+    const page = profileUser.id + 1;
+
+    const [fetchFriendPosts, isPostsLoading, postsError] = useFetching(async () => {
+        const response = await QueryPosts.getAll(limit, page);
+        setPosts([...posts, ...response.data]);
+    })
+    useEffect(() => {
+        fetchFriendPosts();
+    }, [])
+
     return (
         <div className={classes.profile}>
             <div className={classes.photo}><img className={classes.photo} src={profileUser.image} alt="Picture" /></div>
-                <div className={classes.description}>
-                    <h2>{profileUser.surname} {profileUser.name}</h2>
-                    <h4>Age: {profileUser.age} years</h4>
-                    <h4>City: {profileUser.city}</h4>
-                </div>
-                <div className={classes.posts}>Posts</div>
+            <div className={classes.description}>
+                <h2>{profileUser.surname} {profileUser.name}</h2>
+                <h4>Age: {profileUser.age} years</h4>
+                <h4>City: {profileUser.city}</h4>
+            </div>
+            <div className={classes.posts}>
+                {profileUser.posts.length === 0
+                    ?
+                    <div>
+                        {isPostsLoading ? <h3>Идёт загрузка...</h3> : <></>}
+                        {postsError && <h2>Fail - {postsError}</h2>}
+                        {posts.map((post: IPost) =>
+                            <div key={post.id}>
+                                <div className={classes.post}>
+                                    <img src={profileUser.image} className={classes.ava} />
+                                    <div>
+                                        <div className={classes.title}>{post.title}</div>
+                                        <div className={classes.body}>{post.body}</div>
+                                    </div>
+                                </div>
+                            </div>)}
+                    </div>
+                    : <div>
+                        {profileUser.posts.map((post: IPost) =>
+                            <div key={post.id}>
+                                <div className={classes.post}>
+                                    <img src={profileUser.image} className={classes.ava} />
+                                    <div>
+                                        <div className={classes.title}>{post.title}</div>
+                                        <div className={classes.body}>{post.body}</div>
+                                    </div>
+                                </div>
+                            </div>)}
+                    </div>}
+            </div>
         </div>
     );
 };
--- src/redux/FriendSlice.ts
+++ src/redux/FriendSlice.ts
@@ -8,13 +8,23 @@ interface FriendState {
 
 const initialState: FriendState = {
     friends: [
-        {id: 1, name: 'Luda', surname: 'Ionova', city: 'Moscow', age: 22, image: '/images/friend1.png', messages: [
+        {id: 1, name: 'Luda', surname: 'Ionova', city: 'Moscow', age: 22, image: '/images/friend1.png', 
+        messages: [
             {id: 1, message: 'Hello!'}, {id: 2, message: 'My name is Roman'}, {id: 3, message: 'How do you do?'}
+        ], 
+        posts: [
+            {id: 1, title: 'The world is wonderful!', body: 'Today is sunny day. The weather is georgeous'},
+            {id: 2, title: 'The green is my favorite color.', body: 'Tommorow will be sunny day. The weather will be georgeous. ad mollitia et omnis minus architecto odit voluptas doloremque maxime aut non ipsa qui alias veniam blanditiis culpa aut quia nihil cumque facere et occaecati qui aspernatur quia eaque ut aperiam inventore'},
+            {id: 3, title: 'There is a light on the window.', body: 'I want on the out. The weather is georgeous. ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit'},
+            {id: 4, title: 'The blue is my second favorite.', body: 'Tommorow will be sunny day. The weather will be georgeous'},
+            {id: 5, title: 'There is a light on the door.', body: 'I want on the out. The weather is georgeous'},
+            {id: 6, title: 'The dog is my favorite animal.', body: 'Tommorow will be sunny day. The weather will be georgeous. ad mollitia et omnis minus architecto odit voluptas doloremque maxime aut non ipsa qui alias veniam blanditiis culpa aut quia nihil cumque facere et occaecati qui aspernatur quia eaque ut aperiam inventore'},
+            {id: 7, title: 'There is a light on the street.', body: 'I want on the out. The weather is georgeous. ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit'},
         ]},
-        {id: 2, name: 'Egor', surname: 'Sidorov', city: 'Volgograd', age: 18, image: '/images/friend2.png', messages: []},
-        {id: 3, name: 'Anton', surname: 'Petriv', city: 'Kazan', age: 32, image: '/images/friend3.png', messages: []},
-        {id: 4, name: 'Vitaly', surname: 'Alekseev', city: 'Kazan', age: 25, image: '/images/friend4.png', messages: []},
-        {id: 5, name: 'Svetlana', surname: 'Voronova', city: 'Moscow', age: 41, image: '/images/friend5.png', messages: []},
+        {id: 2, name: 'Egor', surname: 'Sidorov', city: 'Volgograd', age: 18, image: '/images/friend2.png', messages: [], posts: []},
+        {id: 3, name: 'Anton', surname: 'Petriv', city: 'Kazan', age: 32, image: '/images/friend3.png', messages: [], posts: []},
+        {id: 4, name: 'Vitaly', surname: 'Alekseev', city: 'Kazan', age: 25, image: '/images/friend4.png', messages: [], posts: []},
+        {id: 5, name: 'Svetlana', surname: 'Voronova', city: 'Moscow', age: 41, image: '/images/friend5.png', messages: [], posts: []},
     ]
 }
 
